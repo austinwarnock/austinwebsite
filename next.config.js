72c75747cf6c806b185f32aa8c997396d3d329eb
Austin Warnock <austinwarnock2017@gmail.com>
2023-08-21 13:40:48 -0500

    fixed build errors

--- next.config.js
+++ next.config.js
@@ -13,6 +13,17 @@ const nextConfig = {
       rule.include = undefined;
     });
 
+    let modularizeImports = null;
+      config.module.rules.some((rule) =>
+        rule.oneOf?.some((oneOf) => {
+          modularizeImports =
+            oneOf?.use?.options?.nextConfig?.modularizeImports;
+          return modularizeImports;
+        }),
+      );
+      if (modularizeImports?.["@headlessui/react"])
+        delete modularizeImports["@headlessui/react"];
+
     return config;
   },
   compress: true,
